ccd321e40044f05d0124b6bf85a8dd8588313259
Kabelo M'sobomvu Moiloa <personal@kabelom.com>
2024-11-27 22:15:01 +0200

    Finish §3.2 The Existential Quantifier

--- MIL/C03_Logic/S02_The_Existential_Quantifier.lean
+++ MIL/C03_Logic/S02_The_Existential_Quantifier.lean
@@ -52,10 +52,18 @@ example (ubf : FnHasUb f) (ubg : FnHasUb g) : FnHasUb fun x ↦ f x + g x := by
   apply fnUb_add ubfa ubgb
 
 example (lbf : FnHasLb f) (lbg : FnHasLb g) : FnHasLb fun x ↦ f x + g x := by
-  sorry
+  rcases lbf with ⟨a, lbfa⟩
+  rcases lbg with ⟨b, lbgb⟩
+  use a + b
+  intro x
+  apply add_le_add (lbfa x) (lbgb x)
 
 example {c : ℝ} (ubf : FnHasUb f) (h : c ≥ 0) : FnHasUb fun x ↦ c * f x := by
-  sorry
+  rcases ubf with ⟨y, d⟩
+  use c * y
+  intro x
+  exact mul_le_mul_of_nonneg_left (d x) h
+
 
 example : FnHasUb f → FnHasUb g → FnHasUb fun x ↦ f x + g x := by
   rintro ⟨a, ubfa⟩ ⟨b, ubgb⟩
@@ -129,7 +137,11 @@ example (divab : a ∣ b) (divbc : b ∣ c) : a ∣ c := by
   use d * e; ring
 
 example (divab : a ∣ b) (divac : a ∣ c) : a ∣ b + c := by
-  sorry
+  rcases divab with ⟨d₁, p₁⟩
+  rcases divac with ⟨d₂, p₂⟩
+  rw [p₁, p₂]
+  use d₁ + d₂
+  rw [mul_add]
 
 end
 
@@ -143,7 +155,9 @@ example {c : ℝ} : Surjective fun x ↦ x + c := by
   dsimp; ring
 
 example {c : ℝ} (h : c ≠ 0) : Surjective fun x ↦ c * x := by
-  sorry
+  intro y
+  use y / c
+  field_simp
 
 example (x y : ℝ) (h : x - y ≠ 0) : (x ^ 2 - y ^ 2) / (x - y) = x + y := by
   field_simp [h]
@@ -163,6 +177,9 @@ variable {α : Type*} {β : Type*} {γ : Type*}
 variable {g : β → γ} {f : α → β}
 
 example (surjg : Surjective g) (surjf : Surjective f) : Surjective fun x ↦ g (f x) := by
-  sorry
+  intro y
+  rcases surjg y with ⟨x₂, rfl⟩
+  rcases surjf x₂ with ⟨x₁, rfl⟩
+  use x₁
 
 end
