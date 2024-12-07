24a9006476fd7f1bf0fa4c26d7c0cdaa522b4851
Kabelo M'sobomvu Moiloa <personal@kabelom.com>
2024-12-07 21:37:00 +0200

    Finish §3.5 Disjunction

--- MIL/C03_Logic/S05_Disjunction.lean
+++ MIL/C03_Logic/S05_Disjunction.lean
@@ -58,19 +58,41 @@ example : x < |y| → x < y ∨ x < -y := by
 namespace MyAbs
 
 theorem le_abs_self (x : ℝ) : x ≤ |x| := by
-  sorry
+  rcases le_or_gt 0 x with h | h
+  . rw [abs_of_nonneg h]
+  . rw [abs_of_neg h]; linarith
 
 theorem neg_le_abs_self (x : ℝ) : -x ≤ |x| := by
-  sorry
+  rcases le_or_gt 0 x with h | h
+  . rw [abs_of_nonneg h]; linarith
+  . rw [abs_of_neg h]
 
 theorem abs_add (x y : ℝ) : |x + y| ≤ |x| + |y| := by
-  sorry
+  rcases le_or_gt 0 (x + y) with h | h
+  . rw [abs_of_nonneg h]; exact add_le_add (le_abs_self x) (le_abs_self y)
+  . rw [abs_of_neg h]
+    rw [neg_add x y]
+    exact add_le_add (neg_le_abs_self x) (neg_le_abs_self y)
 
 theorem lt_abs : x < |y| ↔ x < y ∨ x < -y := by
-  sorry
+  constructor
+  . intro xltabsy
+    rcases le_or_gt 0 y with h | h
+    . rw [abs_of_nonneg h] at xltabsy; left; assumption
+    . rw [abs_of_neg h] at xltabsy; right; assumption
+  . rintro (xlty | xlt_negy)
+    . exact lt_of_lt_of_le xlty (le_abs_self y)
+    . exact lt_of_lt_of_le xlt_negy (neg_le_abs_self y)
 
 theorem abs_lt : |x| < y ↔ -y < x ∧ x < y := by
-  sorry
+  rw [neg_lt]
+  rcases le_or_gt 0 x with h | h
+  . rw [abs_of_nonneg h]
+    rw [and_comm, iff_self_and]
+    intro; linarith
+  . rw [abs_of_neg h]
+    rw [iff_self_and]
+    intro; linarith
 
 end MyAbs
 
@@ -91,23 +113,45 @@ example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
     apply dvd_mul_right
 
 example {z : ℝ} (h : ∃ x y, z = x ^ 2 + y ^ 2 ∨ z = x ^ 2 + y ^ 2 + 1) : z ≥ 0 := by
-  sorry
+  rcases h with ⟨x,y, (p | p)⟩ <;> (rw [p]; positivity)
+
 
 example {x : ℝ} (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  rw [←sub_eq_zero] at h
+  have : x^2 - 1 = (x - 1) * (x + 1) := by ring
+  rw [this] at h
+  have : x - 1 = 0 ∨ x + 1 = 0 := mul_eq_zero.mp h
+  rw [sub_eq_zero, ←eq_neg_iff_add_eq_zero] at this
+  exact this
 
 example {x y : ℝ} (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  rw [←sub_eq_zero] at h
+  have : x^2 - y^2 = (x - y) * (x + y) := by ring
+  rw [this] at h
+  have : x - y = 0 ∨ x + y = 0 := mul_eq_zero.mp h
+  rw [sub_eq_zero, ←eq_neg_iff_add_eq_zero] at this
+  exact this
 
 section
 variable {R : Type*} [CommRing R] [IsDomain R]
 variable (x y : R)
 
 example (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  rw [←sub_eq_zero] at h
+  have : x^2 - 1 = (x - 1) * (x + 1) := by ring
+  rw [this] at h
+  have : x - 1 = 0 ∨ x + 1 = 0 := mul_eq_zero.mp h
+  rw [sub_eq_zero, ←eq_neg_iff_add_eq_zero] at this
+  exact this
+
 
 example (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  rw [←sub_eq_zero] at h
+  have : x^2 - y^2 = (x - y) * (x + y) := by ring
+  rw [this] at h
+  have : x - y = 0 ∨ x + y = 0 := mul_eq_zero.mp h
+  rw [sub_eq_zero, ←eq_neg_iff_add_eq_zero] at this
+  exact this
 
 end
 
@@ -124,5 +168,11 @@ example (P : Prop) : ¬¬P → P := by
   contradiction
 
 example (P Q : Prop) : P → Q ↔ ¬P ∨ Q := by
-  sorry
-
+  constructor
+  . rintro h
+    by_cases h' : P
+    . right; exact h h'
+    . left; assumption
+  . rintro (hnotP | Q)
+    . intro; contradiction
+    . intro; assumption
